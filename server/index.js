73995d4c4fbc7bd5f1d432cd5869564953ac45eb
unknown <alemannof43@gmail.com>
2025-03-27 14:42:13 +0100

    ..

--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
-const express= require('express')
+import express, { json } from 'express'
+
+
 const app = express();
 const port = 5001;
-app.use("/", (req, res) => {
-  res.send("server is runnin");
+
+app.use(json())
+
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await db.manyOrNone(`SELECT * FROM users`);
+    if (!users) {
+      res.status(400).json({ message: "utenti non trovati" });
+      return;
+    }
+    
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "errore nel server" });
+  }
 });
 
 app.listen(port, () => {
